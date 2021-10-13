55e5a7b8dd060be496f9d434f7e5423be19239e7
noam-g4 <noam.g4@gmail.com>
2021-10-13 14:29:15 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import reactDOM from 'react-dom'
+
+reactDOM.render(<h1>hello world</h1>, document.querySelector('#root'))
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+module.exports = {
+  entry: path.join(__dirname, 'src/index.js'),
+
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'public'),
+  },
+
+  mode: 'development',
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    port: 3000,
+  },
+
+  module: {
+    rules: [
+      { test: /\.(js)$/, exclude: /node_modules/, use: ['babel-loader'] },
+    ],
+  },
+}
